b5d03581df43a6ff19c9fc807236bc05b2ec619b
jisoo <dlwltn0350@naver.com>
2022-08-03 08:40:42 +0900

    [0802] BOJ_01254 수정완

--- src/BOJ/구현/BOJ_01254.java
+++ src/BOJ/구현/BOJ_01254.java
@@ -1,5 +1,10 @@
 package BOJ.구현;
-
+/**
+ * @author jisoo
+ * @see https://www.acmicpc.net/problem/1254 팰린드롬 만들기
+ * @performance 14340	128
+ * @category #문자열 #브루트포스
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,21 +19,23 @@ public class BOJ_01254 {
 		String str = br.readLine();
 		sb.append(str);
 		
-		if(sb.toString().equals(sb.reverse().toString())) {
+		if(sb.toString().equals(new StringBuilder(sb.toString()).reverse().toString())) {
 			System.out.println(str.length());
 		}
 		else {
 			for(int i=1; i<str.length();i++) {
 				sb.append(new StringBuilder(str.substring(0,i)).reverse().toString());
-				System.out.println(sb.toString());
-				if(sb.toString().equals(sb.reverse().toString())) {
+				//System.out.println(i+" : "+sb.toString());
+				if(sb.toString().equals(new StringBuilder(sb.toString()).reverse().toString())) {
 					System.out.println(sb.length());
 					break;
 				}
+				sb.delete(0, sb.length());//전체 삭제
+				sb.append(str);
 			}
 		}
-//		System.out.println(sb.toString());
-//		System.out.println(sb.length());
+		//System.out.println(sb.toString());
+		//System.out.println(sb.length());
 	}
 
 }
