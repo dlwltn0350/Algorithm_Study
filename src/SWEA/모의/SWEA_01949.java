ab1efa3c67adbea5f740e1d41231fda66f8f6211
jisoo <dlwltn0350@naver.com>
2022-10-04 00:29:46 +0900

    swea

--- src/SWEA/모의/SWEA_01949.java
+++ src/SWEA/모의/SWEA_01949.java
@@ -1,4 +1,4 @@
-package SWEA.그래프;
+package SWEA.모의;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/SWEA/모의/SWEA_01953.java
+++ src/SWEA/모의/SWEA_01953.java
@@ -1,4 +1,4 @@
-package SWEA.구현;
+package SWEA.모의;
 
 import java.io.BufferedReader;
 import java.io.IOException;
