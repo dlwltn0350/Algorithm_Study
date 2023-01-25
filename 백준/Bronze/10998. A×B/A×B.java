ebdfc3846fd8947c8510f9941a5cf3f42a1741fb
Jisoo Lee <56522878+dlwltn0350@users.noreply.github.com>
2023-01-26 05:12:35 +0900

    [Bronze V] Title: A×B, Time: 76 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+/**
+
+@author jisoo
+@since 2023. 1. 26.
+@see
+@performance
+@category #
+@note */
+public class Main {
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer tokens;
+	
+	public static void main(String[] args) throws IOException {
+		tokens = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(tokens.nextToken());
+		int B = Integer.parseInt(tokens.nextToken());
+		System.out.println(A*B);
+	}
+}
