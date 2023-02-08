f86581e9426a7c8bf08dcd65906b72bef60aa2d2
Jisoo Lee <56522878+dlwltn0350@users.noreply.github.com>
2023-02-09 00:01:45 +0900

    [Bronze V] Title: N 찍기, Time: 1580 ms, Memory: 31268 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<N+1; i++){
+            System.out.println(i);
+        }
+    }
+}
