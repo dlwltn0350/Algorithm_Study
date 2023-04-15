0aee4cb2406e9d49649845c5507470aae5bf76b3
Jisoo Lee <56522878+dlwltn0350@users.noreply.github.com>
2023-04-15 19:06:14 +0900

    [Silver V] Title: 집합, Time: 972 ms, Memory: 324500 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -3,16 +3,22 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * 비트마스킹 풀이
+ * x y and or
+ * 1 1 1 1
+ * 1 0 0 1
+ * 0 1 0 1
+ * 0 0 0 0
+ */
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer tokens;
     static StringBuilder sb = new StringBuilder();
-    static boolean[] visited;
 
     public static void main(String[] args) throws Exception {
         int M = Integer.parseInt(br.readLine());
-        visited = new boolean[21];
-        Arrays.fill(visited,false);
+        int S = 0; //비트마스킹 용도
 
         for(int i=0; i<M; i++){
             tokens = new StringTokenizer(br.readLine());
@@ -20,30 +26,26 @@ public class Main {
             int num = 0;
             if(str.equals("add")){
                 num = Integer.parseInt(tokens.nextToken());
-                visited[num] = true;
+                S = S | (1<<num); //추가 (OR연산)
+                //num번째에 1을 추가한다
             }else if(str.equals("remove")){
                 num = Integer.parseInt(tokens.nextToken());
-                visited[num] = false;
+                S = S & ~(1<<num); //and연산
             }else if(str.equals("check")){
                 num = Integer.parseInt(tokens.nextToken());
-                if(visited[num]){
-                    sb.append(1).append("\n");
-                }else{
+                if((S&(1<<num)) == 0){
                     sb.append(0).append("\n");
+                }else{ //그자리에 1일 경우에만 맞다
+                    sb.append(1).append("\n");
                 }
             }else if(str.equals("toggle")){
                 num = Integer.parseInt(tokens.nextToken());
-                if(visited[num])
-                    visited[num]= false;
-                else visited[num] = true;
+                S ^= (1<<num);
+                //A^B => 둘이 같으면 1 다르면 0
             }else if(str.equals("all")){
-                for(int j=1; j<visited.length; j++){
-                    visited[j] = true;
-                }
+                S = (1<<21) -1;
             }else if(str.equals("empty")){
-                for(int j=1; j<visited.length; j++){
-                    visited[j] = false;
-                }
+                S = 0;
             }
         }
 
